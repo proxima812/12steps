379a7df5b8cdf1aa4e9700aeba645d74ba1df7fc
proxima812 <mrzuc2015@yandex.ru>
2023-12-17 12:41:55 +0600

    fix problem

--- src/pages/posts/[slug]/index.png.ts
+++ src/pages/posts/[slug]/index.png.ts
@@ -1,20 +1,20 @@
-import { generateOgImageForPost } from "@/utils/generateOgImages.tsx";
-import type { APIRoute } from "astro";
-import { getCollection, type CollectionEntry } from "astro:content";
+// import { generateOgImageForPost } from "@/utils/generateOgImages.tsx";
+// import type { APIRoute } from "astro";
+// import { getCollection, type CollectionEntry } from "astro:content";
 
-export async function getStaticPaths() {
-  const posts = await getCollection("posts");
+// export async function getStaticPaths() {
+//   const posts = await getCollection("posts");
 
-  return posts.map((post) => ({
-    params: { slug: post.slug },
-    props: post,
-  }));
-}
+//   return posts.map((post) => ({
+//     params: { slug: post.slug },
+//     props: post,
+//   }));
+// }
 
-export const GET: APIRoute = async ({ props }) =>
-  new Response(
-    await generateOgImageForPost(props as CollectionEntry<"posts">),
-    {
-      headers: { "Content-Type": "image/png" },
-    },
-  );
+// export const GET: APIRoute = async ({ props }) =>
+//   new Response(
+//     await generateOgImageForPost(props as CollectionEntry<"posts">),
+//     {
+//       headers: { "Content-Type": "image/png" },
+//     },
+//   );
--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -1,45 +1,45 @@
-import { Resvg } from "@resvg/resvg-js";
-import { type CollectionEntry } from "astro:content";
-import satori, { type SatoriOptions } from "satori";
-import postOgImage from "./og-templates/post";
+// import { Resvg } from "@resvg/resvg-js";
+// import { type CollectionEntry } from "astro:content";
+// import satori, { type SatoriOptions } from "satori";
+// import postOgImage from "./og-templates/post";
 
-const fetchFonts = async () => {
-  const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/open-sans.bold.ttf",
-  );
-  const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
-  return { fontBold };
-};
+// const fetchFonts = async () => {
+//   const fontFileBold = await fetch(
+//     "https://www.1001fonts.com/download/font/open-sans.bold.ttf",
+//   );
+//   const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
+//   return { fontBold };
+// };
 
-const { fontBold } = await fetchFonts();
+// const { fontBold } = await fetchFonts();
 
-const options: SatoriOptions = {
-  width: 1200,
-  height: 630,
-  embedFont: true,
-  fonts: [
-    // {
-    //   name: "IBM Plex Mono",
-    //   data: fontRegular,
-    //   weight: 400,
-    //   style: "normal",
-    // },
-    {
-      name: "Inter",
-      data: fontBold,
-      weight: 700,
-      style: "normal",
-    },
-  ],
-};
+// const options: SatoriOptions = {
+//   width: 1200,
+//   height: 630,
+//   embedFont: true,
+//   fonts: [
+//     // {
+//     //   name: "IBM Plex Mono",
+//     //   data: fontRegular,
+//     //   weight: 400,
+//     //   style: "normal",
+//     // },
+//     {
+//       name: "Inter",
+//       data: fontBold,
+//       weight: 700,
+//       style: "normal",
+//     },
+//   ],
+// };
 
-function svgBufferToPngBuffer(svg: string) {
-  const resvg = new Resvg(svg);
-  const pngData = resvg.render();
-  return pngData.asPng();
-}
+// function svgBufferToPngBuffer(svg: string) {
+//   const resvg = new Resvg(svg);
+//   const pngData = resvg.render();
+//   return pngData.asPng();
+// }
 
-export async function generateOgImageForPost(post: CollectionEntry<"posts">) {
-  const svg = await satori(postOgImage(post), options);
-  return svgBufferToPngBuffer(svg);
-}
+// export async function generateOgImageForPost(post: CollectionEntry<"posts">) {
+//   const svg = await satori(postOgImage(post), options);
+//   return svgBufferToPngBuffer(svg);
+// }
