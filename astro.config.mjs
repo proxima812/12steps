9677cfcbadcfc290f1d06f495659d8d44a6579d1
proxima812 <mrzuc2015@yandex.ru>
2023-12-30 01:00:00 +0600

    23

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import markdoc from "@astrojs/markdoc";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import keystatic from "@keystatic/astro";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/config/settings";
-
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,12 +18,13 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sitemap({
-        filter: (page) => !page.includes("keystatic"),
-      }),
+      filter: (page) => !page.includes("keystatic"),
+    }),
     mdx(),
     react(),
     markdoc(),
     keystatic(),
+    icon(),
     partytown(
       partytown({
         // Example: Add dataLayer.push as a forwarding-event.
